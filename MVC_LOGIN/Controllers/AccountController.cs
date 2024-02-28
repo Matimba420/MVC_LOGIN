9aacb45ea6cc9d5dd0647342e06d9c47e81f98de
Matimba <matimbamanyondos@Gmail.com>
2024-02-28 23:26:25 +0200

    modify

---
+++ MVC_LOGIN/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_LOGIN.Models;
+using System.Data.SqlClient;
+namespace MVC_LOGIN.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        // GET: Account
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        void connectionString()
+        {
+            con.ConnectionString = "data source=(localdb)MSSQLLocalDB, database=WPF; intergrate security= SSPI;";
+        }
+
+        public ActionResult Verify(Account acc)
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from dbo.Dbo.tbl1_login where username='"+acc.Name+"' and password='"+acc.password+"'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                return View();
+            }
+            else
+            {
+                con.Close();
+                return View();
+            }
+            
+            
+        }
+    }
+}
---
+++ MVC_LOGIN/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_LOGIN.Models
+{
+    public class Account
+    {
+        public string Name { get; set; }
+        public string password { get; set; }
+
+    }
+}
